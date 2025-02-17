ffdf1647e4acc93b3587a40bdc23b9dd98008a26
Paul <paulhuwiler.info@gmail.com>
2025-02-17 22:56:48 +0100

    update

--- main.go
+++ main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gencert/cert"
 	"gencert/certhtml"
+	"gencert/certpdf"
 	"os"
 )
 
 func main() {
+	outputType := flag.String("type", "pdf", "output type of the certificate")
+	flag.Parse()
+
+	var saver cert.Saver
+	var err error
+	switch *outputType {
+	case "html":
+		saver, err = certhtml.New("Output") // not working ?
+	case "pdf":
+		saver, err = certpdf.New("output") // not working ?
+	default:
+		fmt.Println("Unknown output type")
+
+	}
+
+	if err != nil {
+		fmt.Println("")
+		os.Exit(1)
+	}
+
 	c, err := cert.New("le plus bo", "Simon Frey", "2025-02-12")
 	if err != nil {
 		fmt.Printf("Error0")
-		os.Exit(1)
-	}
-	//var saver cert.Saver
-	//saver, err := certpdf.New("output")
-	saver, err := certhtml.New("output")
-	if err != nil {
-		fmt.Println("Error1")
-		panic(err)
-
 		os.Exit(1)
 	}
 	saver.Save(*c)
